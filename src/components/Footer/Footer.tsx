d8efac84caa6d2f8fa5d08d9f9a097d30930e57b
hix <canbedir100@gmail.com>
2024-08-02 03:49:53 +0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaTiktok,
+  FaTwitter,
+  FaX,
+} from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full h-24 border-t border-gray-700 flex flex-col gap-5 items-center justify-center mt-3 md:mt-10">
+      <div className="flex items-center justify-center gap-10">
+        <FaInstagram size={30}/>
+        <FaX size={30}/>
+        <FaTiktok size={30}/>
+        <FaLinkedin size={30}/>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
