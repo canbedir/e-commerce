83da673375aa50e6a0dfb36f6e20143f1f82aee1
hix <canbedir100@gmail.com>
2024-08-05 18:33:16 +0300

    style: Add border to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import { getCurrentUser } from '@/app/actions/getCurrentUser';
 const Navbar = async () => {
   const currentUser = await getCurrentUser();
   return (
-    <div className='container mx-auto px-4 md:px-10 flex items-center justify-between h-16'>
+    <div className='container mx-auto px-4 md:px-10 flex items-center justify-between h-16 border-b border-black/50'>
       <div className='flex items-center gap-3 md:gap-10 w-full'>
         <Logo />
         <Search />
