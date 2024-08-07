1bdb6ab53c899897160eb073cc0c2eda03d92f67
hix <canbedir100@gmail.com>
2024-08-07 18:56:56 +0300

    style: Update logo styles

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
     <div>
       <Link href={"/"}>
-        <Image src={"/xlogo.png"} alt="logo" width={50} height={50}/>
+        <div className="flex items-center -space-x-2">
+          <h1 className="text-white text-2xl">hi</h1>
+          <Image src={"/xlogo.png"} alt="logo" width={50} height={50} className="object-contain" />
+        </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
