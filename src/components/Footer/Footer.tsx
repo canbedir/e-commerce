16d06ed4cef3e21c4eb2309ff5684ae3b2fbeaf1
hix <canbedir100@gmail.com>
2024-08-08 00:22:34 +0300

    style: Change footer icon colors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,12 +9,12 @@ import {
 
 const Footer = () => {
   return (
-    <div className="w-full h-24 border-t border-gray-700 flex flex-col gap-5 items-center justify-center mt-3 md:mt-10">
+    <div className="w-full h-24 text-white border-t border-gray-700 flex flex-col gap-5 items-center justify-center mt-3 md:mt-10">
       <div className="flex items-center justify-center gap-10">
-        <FaInstagram size={30}/>
-        <FaX size={30}/>
-        <FaTiktok size={30}/>
-        <FaLinkedin size={30}/>
+        <FaInstagram className="cursor-pointer" size={30}/>
+        <FaX className="cursor-pointer" size={30}/>
+        <FaTiktok className="cursor-pointer" size={30}/>
+        <FaLinkedin className="cursor-pointer" size={30}/>
       </div>
     </div>
   );
