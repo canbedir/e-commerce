3991d75a80f5d005d15a8676dafb096ffdac7f87
hix <canbedir100@gmail.com>
2024-08-07 19:30:28 +0300

    style: Update Product Card styles

--- src/components/Home/ProductCard.tsx
+++ src/components/Home/ProductCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Rating } from "@mui/material";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -11,16 +12,16 @@ const ProductCard = ({ product }: { product: any }) => {
   return (
     <div
       onClick={() => router.push(`product/${product.id}`)}
-      className="w-[240px] cursor-pointer shadow-lg p-2 rounded-md bg-slate-200 hover:bg-slate-300 flex flex-col justify-between"
+      className="w-[240px] cursor-pointer shadow-lg p-2 rounded-md border text-white border-white bg-zinc-900 hover:bg-zinc-800 flex flex-col justify-between"
     >
       <div className="relative h-[180px]">
         <Image src={product.image} alt="" fill className="object-contain" />
       </div>
       <div className="mt-2 flex flex-col items-start gap-1 flex-grow">
         <div className="flex gap-2">
-          <span className="text-black font-bold line-clamp-2">
+          <span className="font-bold line-clamp-2">
             {product.brand}
-            <span className="text-black font-normal ml-2">{product.name}</span>
+            <span className="font-normal ml-2">{product.name}</span>
           </span>
         </div>
         <div className="flex text-xs gap-1">
@@ -29,12 +30,13 @@ const ProductCard = ({ product }: { product: any }) => {
             value={product.value}
             readOnly
             className="text-lg"
+            emptyIcon={<StarIcon style={{color:"gray"}}/>}
           />
           <span className="text-slate-500">({product.evaluation})</span>
         </div>
       </div>
-      <div className="text-red-700 font-semibold text-lg md:text-xl mt-2 self-start">
-        {product.price} ₺
+      <div className="font-semibold text-lg md:text-xl mt-2 self-start">
+        {product.price} <span className="text-green-600">₺</span>
       </div>
     </div>
   );
