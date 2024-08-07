2ea32420339c820726c46b5872455d23c72cd8aa
hix <canbedir100@gmail.com>
2024-08-07 15:19:53 +0300

    authenticator updated

--- src/app/api/product/[productId]/route.ts
+++ src/app/api/product/[productId]/route.ts
@@ -1,23 +1,46 @@
-import prisma from "@/lib/prismadb"
+import prisma from "@/lib/prismadb";
 import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 
-export async function DELETE(request: Request, { params }: { params: { productId: string } }) {
-    const currentUser = await getCurrentUser()
+export async function DELETE(
+  request: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    const currentUser = await getCurrentUser();
 
     if (!currentUser || currentUser.role !== "ADMIN") {
-        return NextResponse.error()
+      return new NextResponse(null, {
+        status: 403,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
     }
 
-    try {
-        const product = await prisma.product.delete({
-            where: {
-                id: params.productId,
-            },
-        });
-        return NextResponse.json(product);
-    } catch (error) {
-        console.error("Error deleting product: ", error);
-        return NextResponse.error();
-    }
+    const product = await prisma.product.delete({
+      where: {
+        id: params.productId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(product), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error deleting product: ", error);
+
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
