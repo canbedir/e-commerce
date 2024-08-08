af04edd16d0079a7409caa9028ae1af46a93ae70
hix <canbedir100@gmail.com>
2024-08-09 02:17:22 +0300

    style: Change footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import {
   FaInstagram,
@@ -9,12 +10,36 @@ import {
 
 const Footer = () => {
   return (
-    <div className="w-full h-24 text-white border-t border-gray-700 flex flex-col gap-5 items-center justify-center mt-3 md:mt-10">
-      <div className="flex items-center justify-center gap-10">
-        <FaInstagram className="cursor-pointer" size={30}/>
-        <FaX className="cursor-pointer" size={30}/>
-        <FaTiktok className="cursor-pointer" size={30}/>
-        <FaLinkedin className="cursor-pointer" size={30}/>
+    <div className="w-full border-white/20 border-t mt-40">
+      <div className="py-10 px-20 text-white md:flex md:items-center md:justify-between">
+        <div className="text-center md:text-left">
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} All Rights Reserved
+          </p>
+        </div>
+
+        <div className="mt-4 flex items-center justify-center md:mt-0">
+          <div className="flex space-x-8">
+            <Link
+              href="#"
+              className="text-sm  hover:text-white/60"
+            >
+              Terms
+            </Link>
+            <Link
+              href="#"
+              className="text-sm hover:text-white/60"
+            >
+              Privacy Policy
+            </Link>
+            <Link
+              href="#"
+              className="text-sm hover:text-white/60"
+            >
+              Cookie Policy
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
