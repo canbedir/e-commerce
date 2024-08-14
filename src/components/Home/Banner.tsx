c1770130e723f3d72326fe1fea676b83f55bee6e
hix <canbedir100@gmail.com>
2024-08-14 21:54:14 +0300

    feat(banner): implement swiper slider for banner

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,25 +1,57 @@
-"use client"
+"use client";
 import Image from "next/image";
 import React, { useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Swiper CSS dosyasını dahil etmeyi unutmayın.
 
 const Banner = () => {
   const [imageLoaded, setImageLoaded] = useState(false);
 
   return (
-    <div className="h-[437px] hidden md:block">
-      <div className="h-[337px] relative">
+    <div className="h-[500px] hidden md:block">
         {!imageLoaded && <Skeleton className="h-full w-full rounded-md" />}
-        <Image
-          src={"/banner1.jpg"}
-          alt=""
-          fill
-          className={`object-cover transition-opacity duration-500 ${
-            imageLoaded ? "opacity-100" : "opacity-0"
-          }`}
-          onLoadingComplete={() => setImageLoaded(true)}
-        />
-      </div>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1} // Slayt başına gösterilecek resim sayısını 1 yaparak, her slaytın tamamını gösterebilirsiniz.
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="h-[400px] relative"
+        >
+          <SwiperSlide>
+            <Image
+              src={"/banner1.jpg"}
+              alt=""
+              fill
+              className={`object-cover transition-opacity duration-500 ${
+                imageLoaded ? "opacity-100" : "opacity-0"
+              }`}
+              onLoadingComplete={() => setImageLoaded(true)}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={"/banner2.jpg"}
+              alt=""
+              fill
+              className={`object-cover transition-opacity duration-500 ${
+                imageLoaded ? "opacity-100" : "opacity-0"
+              }`}
+              onLoadingComplete={() => setImageLoaded(true)}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image
+              src={"/banner3.jpg"}
+              alt=""
+              fill
+              className={`object-cover transition-opacity duration-500 ${
+                imageLoaded ? "opacity-100" : "opacity-0"
+              }`}
+              onLoadingComplete={() => setImageLoaded(true)}
+            />
+          </SwiperSlide>
+        </Swiper>
     </div>
   );
 };
